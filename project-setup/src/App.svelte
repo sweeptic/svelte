5f35a9be1f0d49ef9c209b842c26d960974333ae
Attila Horvath <sweeptic@gmail.com>
2023-12-01 16:44:08 +0100

    014 Binding to Element Properties

--- project-setup/src/App.svelte
+++ project-setup/src/App.svelte
@@ -8,7 +8,6 @@
 
   $: if (name === 'Maximilian') {
     console.log('runs');
-
     age = 31;
   }
 
@@ -19,12 +18,18 @@
   function changeName() {
     name = 'Maximilian';
   }
+
+  function nameInput(event) {
+    // console.log(event);
+    name = event.target.value;
+  }
 </script>
 
 <h1>Hello {name.toUpperCase()}, {age}!</h1>
 <h1>Hello {uppercaseName}, {age}!</h1>
 <button on:click={incrementAge}>Change Age</button>
 <button on:click={changeName}>Change Age</button>
+<input type="text" value={name} on:input={nameInput} />
 
 <style>
   h1 {
