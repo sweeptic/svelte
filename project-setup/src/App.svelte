2d4c875ecbf2a9e9beeb8210fa83b8b03864d5a2
Attila Horvath <sweeptic@gmail.com>
2023-12-01 16:39:13 +0100

    013 More Reactivity

--- project-setup/src/App.svelte
+++ project-setup/src/App.svelte
@@ -1,14 +1,30 @@
 <script>
-  export let name = 'asdfgh';
+  export let name = 'Max';
   export let age = 12;
+
+  $: uppercaseName = name.toUpperCase(); // label statement
+
+  $: console.log(name);
+
+  $: if (name === 'Maximilian') {
+    console.log('runs');
+
+    age = 31;
+  }
 
   function incrementAge() {
     age++;
   }
+
+  function changeName() {
+    name = 'Maximilian';
+  }
 </script>
 
-<h1>Hello {name}, {age}!</h1>
+<h1>Hello {name.toUpperCase()}, {age}!</h1>
+<h1>Hello {uppercaseName}, {age}!</h1>
 <button on:click={incrementAge}>Change Age</button>
+<button on:click={changeName}>Change Age</button>
 
 <style>
   h1 {
