31baa02de45a0309ae74f54f53d67087c763eaaf
Attila Horvath <sweeptic@gmail.com>
2023-12-02 19:47:21 +0100

    044 Adding Data

--- 05 Course Project -  First Steps/src/App.svelte
+++ 05 Course Project -  First Steps/src/App.svelte
@@ -1,5 +1,9 @@
 <script>
   import Header from './UI/Header.svelte';
+  import someData from './data/data';
+
+  const meetups = someData;
+  console.log('someData', someData);
 </script>
 
 <Header />
