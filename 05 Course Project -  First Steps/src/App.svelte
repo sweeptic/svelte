5de130883f1bf1b3be11c79e27fee05094c1adc8
Attila Horvath <sweeptic@gmail.com>
2023-12-04 14:13:54 +0100

    050 Creating a _TextInput_ Component

--- 05 Course Project -  First Steps/src/App.svelte
+++ 05 Course Project -  First Steps/src/App.svelte
@@ -1,4 +1,5 @@
 <script>
+  import TextInput from './UI/TextInput.svelte';
   import MeetupGrid from './Meetups/MeetupGrid.svelte';
   import Header from './UI/Header.svelte';
   import meetupsData from './data/data';
@@ -30,30 +31,49 @@
 <Header />
 <main>
   <form on:submit|preventDefault={addMeetup}>
-    <div class="form-control">
-      <label for="title">title</label>
-      <input type="text" id="title" bind:value={title} />
-    </div>
-    <div class="form-control">
-      <label for="subtitle">subtitle</label>
-      <input type="text" id="subtitle" bind:value={subtitle} />
-    </div>
-    <div class="form-control">
-      <label for="imageUrl">imageUrl</label>
-      <input type="text" id="imageUrl" bind:value={imageUrl} />
-    </div>
-    <div class="form-control">
-      <label for="address">address</label>
-      <input type="text" id="address" bind:value={address} />
-    </div>
-    <div class="form-control">
-      <label for="contactEmail">contactEmail</label>
-      <input type="text" id="contactEmail" bind:value={contactEmail} />
-    </div>
-    <div class="form-control">
-      <label for="description">description</label>
-      <textarea rows="3" id="description" bind:value={description} />
-    </div>
+    <TextInput
+      label="title"
+      id="title"
+      value={title}
+      type="text"
+      on:input={(event) => (title = event.target.value)}
+    />
+    <TextInput
+      label="subtitle"
+      id="subtitle"
+      value={subtitle}
+      type="text"
+      on:input={(event) => (subtitle = event.target.value)}
+    />
+    <TextInput
+      label="imageUrl"
+      id="imageUrl"
+      value={imageUrl}
+      type="text"
+      on:input={(event) => (imageUrl = event.target.value)}
+    />
+    <TextInput
+      label="address"
+      id="address"
+      value={address}
+      type="text"
+      on:input={(event) => (address = event.target.value)}
+    />
+    <TextInput
+      label="contactEmail"
+      id="contactEmail"
+      value={contactEmail}
+      type="email"
+      on:input={(event) => (contactEmail = event.target.value)}
+    />
+    <TextInput
+      label="description"
+      id="description"
+      type="textarea"
+      value={description}
+      on:input={(event) => (description = event.target.value)}
+    />
+
     <button type="submit">Save</button>
   </form>
   <MeetupGrid {meetups} />
@@ -63,36 +83,11 @@
   main {
     position: relative;
     margin-top: 120px;
+    background-color: rgb(255, 219, 219);
   }
   /* your styles go here */
-  input,
-  textarea {
-    display: block;
-    width: 100%;
-    font: inherit;
-    border: none;
-    border-bottom: 2px solid #ccc;
-    border-radius: 3px 3px 0 0;
-    background: white;
-    padding: 0.15rem 0.25rem;
-    transition: border-color 0.1s ease-out;
-  }
-
-  input:focus,
-  textarea:focus {
-    border-color: #e40763;
-    outline: none;
-  }
-
-  label {
-    display: block;
-    margin-bottom: 0.5rem;
-    width: 100%;
-  }
-
-  .form-control {
-    padding: 0.5rem 0;
-    width: 100%;
-    margin: 0.25rem 0;
+  form {
+    width: 50%;
+    margin: auto;
   }
 </style>
---
+++ 05 Course Project -  First Steps/src/UI/TextInput.svelte
@@ -0,0 +1,52 @@
+<script>
+  // your script goes here
+
+  export let id;
+  export let label;
+  export let value;
+  export let type;
+</script>
+
+<div class="form-control">
+  <label for={label}>{label}</label>
+  {#if type === 'textarea'}
+    <!-- content here -->
+    <textarea rows="3" {id} {value} on:input />
+  {:else}
+    <!-- else content here -->
+    <input {type} {id} {value} on:input />
+  {/if}
+</div>
+
+<style>
+  input,
+  textarea {
+    display: block;
+    width: 100%;
+    font: inherit;
+    border: none;
+    border-bottom: 2px solid #ccc;
+    border-radius: 3px 3px 0 0;
+    background: white;
+    padding: 0.15rem 0.25rem;
+    transition: border-color 0.1s ease-out;
+  }
+
+  input:focus,
+  textarea:focus {
+    border-color: #e40763;
+    outline: none;
+  }
+
+  label {
+    display: block;
+    margin-bottom: 0.5rem;
+    width: 100%;
+  }
+
+  .form-control {
+    padding: 0.5rem 0;
+    width: 100%;
+    margin: 0.25rem 0;
+  }
+</style>
