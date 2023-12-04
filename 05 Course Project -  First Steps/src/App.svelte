a11d8b40e7f550b66d0cd45c4168ee3dabd16f42
Attila Horvath <sweeptic@gmail.com>
2023-12-04 13:04:21 +0100

    048 Adding a _MeetupGrid_ Component

--- 05 Course Project -  First Steps/src/App.svelte
+++ 05 Course Project -  First Steps/src/App.svelte
@@ -1,11 +1,10 @@
 <script>
-  import MeetupItem from './Meetups/MeetupItem.svelte';
+  import MeetupGrid from './Meetups/MeetupGrid.svelte';
   import Header from './UI/Header.svelte';
   import meetups from './data/data';
 </script>
 
 <Header />
-
-{#each meetups as meetup, i (meetup.id)}
-  <MeetupItem {meetup} />
-{/each}
+<main>
+  <MeetupGrid {meetups} />
+</main>
---
+++ 05 Course Project -  First Steps/src/Meetups/MeetupGrid.svelte
@@ -0,0 +1,25 @@
+<script>
+  import MeetupItem from './MeetupItem.svelte';
+  export let meetups;
+</script>
+
+<section>
+  {#each meetups as meetup, i (meetup.id)}
+    <MeetupItem {meetup} />
+  {/each}
+</section>
+
+<style>
+  section {
+    width: 100%;
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-gap: 1rem;
+  }
+
+  @media (min-width: 768px) {
+    section {
+      grid-template-columns: repeat(2, 1fr);
+    }
+  }
+</style>
