6a93e34cb8bbf84bb72acf3e3826841c97f18bea
Attila Horvath <sweeptic@gmail.com>
2023-12-05 14:22:51 +0100

    082 Two-Way Binding Refresher

---
+++ 08 Working with Bindings & Forms/src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  let value = 'Max';
+
+  $: console.log(value);
+
+  function setValue(event) {
+    value = event.target.value;
+  }
+</script>
+
+<input type="text" {value} on:input={setValue} />
+<!-- <input type="text" bind:value={value} /> -->
+<input type="text" bind:value />
+
+<h1>Bindings & Forms</h1>
