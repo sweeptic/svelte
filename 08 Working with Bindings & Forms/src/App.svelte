f320e07b5f5dc4cb4ffd7c6ba8dcae6d0529399e
Attila Horvath <sweeptic@gmail.com>
2023-12-05 14:38:40 +0100

    083 Understanding Custom Component Bindings

--- 08 Working with Bindings & Forms/src/App.svelte
+++ 08 Working with Bindings & Forms/src/App.svelte
@@ -1,15 +1,23 @@
 <script>
+  import CustomInput from './CustomInput.svelte';
+  import Toggle from './Toggle.svelte';
   let value = 'Max';
+  let selectedOption = 1;
 
   $: console.log(value);
+  $: console.log(selectedOption);
 
   function setValue(event) {
     value = event.target.value;
   }
 </script>
 
-<input type="text" {value} on:input={setValue} />
+<!-- <input type="text" {value} on:input={setValue} /> -->
 <!-- <input type="text" bind:value={value} /> -->
-<input type="text" bind:value />
+<!-- <input type="text" bind:value /> -->
+
+<CustomInput bind:val={value} />
+
+<Toggle bind:chosenOption={selectedOption} />
 
 <h1>Bindings & Forms</h1>
