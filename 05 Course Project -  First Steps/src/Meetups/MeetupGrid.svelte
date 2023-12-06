7ee9044a6e4fc7ce068c33808344c8db125b3f61
Attila Horvath <sweeptic@gmail.com>
2023-12-06 19:03:35 +0100

    123 Adding a _Filter_ Component

--- 05 Course Project -  First Steps/src/Meetups/MeetupGrid.svelte
+++ 05 Course Project -  First Steps/src/Meetups/MeetupGrid.svelte
@@ -1,11 +1,23 @@
 <script>
+  import MeetupFilter from './MeetupFilter.svelte';
   import MeetupItem from './MeetupItem.svelte';
 
   export let meetups;
+  let favsOnly = false;
+
+  function setFilter(event) {
+    favsOnly = event.detail === 1;
+  }
+
+  $: filteredMeetups = favsOnly ? meetups.filter((m) => m.isFavorite) : meetups;
 </script>
 
+<section id="meetup-controls">
+  <MeetupFilter on:select={setFilter} />
+</section>
+
 <section id="meetups">
-  {#each meetups as meetup}
+  {#each filteredMeetups as meetup}
     <MeetupItem
       id={meetup.id}
       title={meetup.title}
@@ -22,15 +34,19 @@
 </section>
 
 <style>
-  section {
+  #meetups {
     width: 100%;
     display: grid;
     grid-template-columns: 1fr;
     grid-gap: 1rem;
   }
 
+  #meetup-controls {
+    margin: 1rem;
+  }
+
   @media (min-width: 768px) {
-    section {
+    #meetups {
       grid-template-columns: repeat(2, 1fr);
     }
   }
