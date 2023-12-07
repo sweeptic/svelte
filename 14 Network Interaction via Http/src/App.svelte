88ae820c2c9b94ca5ccfbe9a207d30dc603e16a3
Attila Horvath <sweeptic@gmail.com>
2023-12-07 12:41:41 +0100

    149 Using the _await_ Block

--- 14 Network Interaction via Http/src/App.svelte
+++ 14 Network Interaction via Http/src/App.svelte
@@ -2,72 +2,86 @@
   import { onMount } from 'svelte';
 
   let hobbies = [];
-  let hobbyInput = `dummyData`;
+  let hobbyInput;
   let isLoading = false;
 
-  //   onMount(() => {
-  // async function loadData() {
-  let data = fetch(
+  isLoading = true;
+  let getHobbies = fetch(
     'https://ng-course-recipe-book-d5b48-default-rtdb.europe-west1.firebasedatabase.app/hobbies.json',
-    { method: 'GET' },
-  );
-
-  //   let respData = await data.json();
-  //   hobbies = Object.values(respData);
-  // }
-  // loadData();
-  //   });
-
-  async function addHobby() {
-    hobbies = [...hobbies, hobbyInput];
-    isLoading = true;
-    try {
-      let res = await fetch(
-        'https://ng-course-recipe-book-d5b48-default-rtdb.europe-west1.firebasedatabase.app/hobbies.json',
-
-        {
-          method: 'POST',
-          body: JSON.stringify(hobbyInput + ' - ' + Math.random()),
-          headers: {
-            'Content-Type': ' application/json',
-          },
-        },
-      );
-
-      if (res.ok) {
-        console.log('res', res);
+  )
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error('Failed!');
       }
-    } catch (error) {
-      console.log('error', error);
-    } finally {
+      return res.json();
+    })
+    .then((data) => {
       isLoading = false;
-    }
+      hobbies = Object.values(data);
+      let keys = Object.keys(data);
+      console.log(keys);
+
+      for (const key in data) {
+        console.log(key, data[key]);
+      }
+      return hobbies;
+    })
+    .catch((err) => {
+      isLoading = false;
+      console.log(err);
+    });
+
+  function addHobby() {
+    hobbies = [...hobbies, hobbyInput.value];
+
+    isLoading = true;
+    fetch(
+      'https://ng-course-recipe-book-d5b48-default-rtdb.europe-west1.firebasedatabase.app/hobbies.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(hobbyInput.value),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+    )
+      .then((res) => {
+        isLoading = false;
+        if (!res.ok) {
+          throw new Error('Failed!');
+        }
+        // ...
+        alert('Saved Data!');
+      })
+      .catch((err) => {
+        isLoading = false;
+        console.log(err);
+      });
   }
 </script>
 
-<!-- https://ng-course-recipe-book-d5b48-default-rtdb.europe-west1.firebasedatabase.app/ -->
-
-<label for="hobby"></label>
-<input type="text" id="hobby" bind:value={hobbyInput} />
+<label for="hobby">Hobby</label>
+<input type="text" id="hobby" bind:this={hobbyInput} />
 <button on:click={addHobby}>Add Hobby</button>
 
 <!-- {#if isLoading}
   <p>Loading...</p>
 {:else}
   <ul>
-    {#each hobbies as hobby (Math.random())}
+    {#each hobbies as hobby}
       <li>{hobby}</li>
     {/each}
   </ul>
 {/if} -->
 
-{#await data}
+{#await getHobbies}
   <p>Loading...</p>
-  <!-- promise is pending -->
 {:then hobbyData}
-  {#each hobbyData as hobby (Math.random())}
-    <li>{hobby}</li>
-  {/each}
+  <ul>
+    {#each hobbyData as hobby}
+      <li>{hobby}</li>
+    {/each}
+  </ul>
 {:catch error}
-  <!-- promise was rejected -->
+  <p>{error.message}</p>
 {/await}
